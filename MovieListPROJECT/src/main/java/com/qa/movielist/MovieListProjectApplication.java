d8891653a96f80608d81c30a80d48a72f310d641
jarrett <joejarrett@outlook.com>
2022-03-21 10:26:04 +0000

    JJ: adding project folder

---
+++ MovieListPROJECT/src/main/java/com/qa/movielist/MovieListProjectApplication.java
@@ -0,0 +1,13 @@
+package com.qa.movielist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieListProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieListProjectApplication.class, args);
+	}
+
+}
